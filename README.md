9d742eb0b33d284a9db969116e8da35ef57186d0
sellauno <sellauno03@gmail.com>
2022-09-03 09:08:51 +0700

    Use an external package

--- README.md
+++ README.md
@@ -2,15 +2,8 @@
 
 A new Flutter project.
 
-## Getting Started
+## Create the starter Flutter app
+![Screenshot](images/01.PNG)
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Use an external package
+![Screenshot](images/02.PNG)
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,14 +10,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(                          // Drop the const, and
+          //child: Text('Hello World'),        // Replace this text...
+          child: Text(wordPair.asPascalCase),  // With this text.
         ),
       ),
     );
